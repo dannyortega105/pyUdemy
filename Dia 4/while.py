2197046e41f4b1aa880891b6880c7855e5d50333
Danny Ortega <ortegadanny.cq@gmail.com>
2022-07-28 22:40:09 -0500

    Clase de bucles While

--- Dia 4/while.py
+++ Dia 4/while.py
@@ -1,4 +1,39 @@
-monedas = 5
+"""monedas = 5
 
 while monedas > 0:
     print(f"Tengo {monedas} monedas")
+    monedas -= 1
+else:
+    print("No tengo mas dinero ")"""
+
+"""respuesta = 's'
+
+while respuesta == 's':
+    respuesta = input("Quieres seguir? (s/n)")
+else:
+    print("Gracias")"""
+
+"""nombre = input("Tu nombre: ")
+
+for letra in nombre:
+    if letra == 'r':
+        continue
+    print(letra)"""
+
+"""numero = 50
+
+while numero >= 0:
+    if numero % 5 == 0:
+        print(numero)
+        numero -= 1
+    else:
+        numero -= 1"""
+
+"""lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3]
+
+for numeros in lista_numeros:
+    if numeros > 0:
+        print(numeros)
+    else:
+        break"""
+
