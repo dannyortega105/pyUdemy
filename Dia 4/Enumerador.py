13cb9b1f1bbcf44a5ba4eaccd9229ea2a3d725a7
Danny Ortega <ortegadanny.cq@gmail.com>
2022-08-01 22:58:01 -0500

    Enumerate and Zip in Day 4

---
+++ Dia 4/Enumerador.py
@@ -0,0 +1,4 @@
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+for i, nombre in lista_nombres:
+    if nombre[0] == 'M':
+        print(i)
---
+++ Dia 4/Zip.py
@@ -0,0 +1,20 @@
+"""nombres = ['Ana','Hugo','Valeria']
+edades = [65, 29, 42]
+ciudades = ['Lima', 'Madrid','Medellin']
+
+combinados = list(zip(nombres, edades, ciudades))
+
+for nombre,edad,ciudad in combinados:
+    print(f"{nombre} tiene {edad} años y vive en {ciudad}") """
+
+#Ejercicio 1
+"""capitales = ["Berlín", "Tokio", "París", "Helsinki", "Ottawa", "Canberra"]
+paises = ["Alemania", "Japón", "Francia", "Finlandia", "Canadá", "Australia"]
+
+frase = list(zip(capitales,paises))
+
+for capital, pais in frase:
+    print(f"La capital de {pais} es {capital}") """
+
+
+
